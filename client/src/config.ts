7c90cc2b7d16f53c9a6467d0718dd0e93537a276
xssssl <pigmaster@sina.com>
2020-06-21 00:38:27 +1000

    add some screenshots

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // Backend API endpoint
-// export const apiEndpoint = 'http://localhost:3003/dev'
-const apiId = '6mjbppeyuf'
-export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
+export const apiEndpoint = 'http://localhost:3003/dev'
+// const apiId = '6mjbppeyuf'
+// export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
 
 // Auth0 configuration
 export const authConfig = {
